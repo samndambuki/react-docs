10a4242259ddde380cbaa5ac5dfdc741ed15a565
samndambuki <sam.ndambuki08@gmail.com>
2023-03-06 11:15:35 +0300

    creating and nesting components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+function MyButton(){
+  return(
+    <button>This is a button</button>
+  ); 
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function MyApp(){
+  return(
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton/>
     </div>
   );
 }
-
-export default App;
